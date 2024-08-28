b3647ec4d0c6b1a64f7f6ef727f0c7b4915ebd02
Braden <baj147@yahoo.com>
2024-08-28 01:11:51 -0400

    setAttritube to ./image/dice4.png

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
-var randomNumber1 = 4
+var randomNumber1 = Math.floor(Math.random() * 6) +1;
+var randomImageSource = "images/dice" + randomNumber1 + ".png";
+var image1 = document.querySelector("img.img1").setAttribute("src", randomImageSource);
 
-document.querySelector("img.img1").setAttribute("src", "./images/dice4.png")
+var randomNumber2 = Math.floor(Math.random() * 6) +1;
+var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+var image2 = document.querySelector("img.img2").setAttribute("src", randomImageSource2)
+
+
+
+    if (randomNumber1 > randomNumber2) {
+         document.querySelector("h1").innerHTML = "Player 1 Wins!";
+    } else if (randomNumber1 < randomNumber2) {
+         document.querySelector("h1").innerHTML = "Player 2 Wins!";
+    } else {
+        document.querySelector("h1").innerHTML = "Draw!";
+    }
+
+
